c39ca00215c3c58637bf3c13757dfed8f979539c
Bohdan Melnyk <bohdan_melnyk@epam.com>
2021-07-30 13:33:53 +0300

    Initial commit

---
+++ app/src/main/java/com/example/generaltask/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.generaltask
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import java.util.*
+
+class MainActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    }
+
